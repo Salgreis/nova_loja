70e778bb9506f2ccb62121962748fc77162b1c1e
unknown <joao.reis.salgueiro@gmail.com>
2022-07-20 23:31:51 -0300

    enviando loja

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+require 'environment.php';
+
+global $config;
+global $db;
+
+$config = array();
+if(ENVIRONMENT == 'development') {
+	define("BASE_URL", "http://localhost/nova_loja/");
+	$config['dbname'] = 'nova_loja';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = 'root';
+} else {
+	define("BASE_URL", "http://localhost/nova_loja/");
+	$config['dbname'] = 'nova_loja';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = 'root';
+}
+try {
+	$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo 'conectou';
+	//code...
+} catch (\Throwable $th) {
+	echo 'sem conexao';
+	//throw $th;
+}
+?>
